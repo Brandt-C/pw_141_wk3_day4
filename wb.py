e54fa014d41bf0f758f1a28c1024d181b7189613
Brandt-C <brandt.carlson11@gmail.com>
2024-03-07 19:52:08 -0600

    first

---
+++ wb.py
@@ -0,0 +1,24 @@
+# Remove vowels
+# Write a function that will remove all vowels from a given string. The function should return a string.
+# Examples:                                                                 
+# Input: 'Joel'
+# Output: 'Jl'                                                     
+# Input: 'Shoha'
+# Output: 'Shh'
+
+
+def vowel_removal(input_string):
+    vowels = "aeiouAEIOU"
+    out_put = []
+    for char in input_string:
+        if char not in vowels:
+            out_put.append(char)
+    return ''.join(out_put)
+
+
+def vowel_removal(stringy):
+    vowels = "aeiouAEIOU"
+    return ''.join([s for s in stringy if s not in vowels ])
+            
+            
+print(vowel_removal("Joel"))
